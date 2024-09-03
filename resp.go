fbf594d709e125673d27943b6aa9a1298a4c21c9
Carlos David Gonzalez Nexans <cdgn.machines@gmail.com>
2024-09-03 19:35:03 -0300

    step 1 serialization

---
+++ resp.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var EmptyError = errors.New("cannot serialize empty error")
+
+var SimpleStringsError = errors.New("cannot serialize simple strings with \\n or \\r")
+
+type RespSerializer struct{}
+
+func (RespSerializer) SerializeString(data string) ([]byte, error) {
+	for c := range data {
+		if c == '\n' || c == '\r' {
+			return nil, SimpleStringsError
+		}
+	}
+
+	result := fmt.Sprintf("+%s\r\n", data)
+	return []byte(result), nil
+}
+
+func (RespSerializer) SerializeInteger(data int64) ([]byte, error) {
+	result := fmt.Sprintf(":%d\r\n", data)
+	return []byte(result), nil
+}
+
+func (RespSerializer) SerializeError(data error) ([]byte, error) {
+	if data == nil {
+		return []byte{}, EmptyError
+	}
+	result := fmt.Sprintf("-%s\r\n", data)
+	return []byte(result), nil
+}
+
+func (RespSerializer) SerializeBulkString(data string) ([]byte, error) {
+	result := fmt.Sprintf("$%d\r\n%s\r\n", len(data), data)
+	return []byte(result), nil
+}
---
+++ resp_test.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+)
+
+func TestRespSerializer_SerializeString(t *testing.T) {
+	cases := []struct {
+		data     string
+		expected []byte
+		err      error
+		message  string
+	}{
+		{
+			data:     "OK",
+			expected: []byte("+OK\r\n"),
+			message:  "Simple string",
+			err:      nil,
+		},
+		{
+			data:     "data with \r\n",
+			expected: nil,
+			message:  "Incorrect simple string",
+			err:      SimpleStringsError,
+		},
+	}
+
+	serializer := RespSerializer{}
+	for _, c := range cases {
+		t.Run(c.message, func(t *testing.T) {
+			actual, err := serializer.SerializeString(c.data)
+			if !errors.Is(err, c.err) || bytes.Compare(actual, c.expected) != 0 {
+				t.Errorf("Actual data %s, actual error %v, expected data %s, expected error %v", actual, err, c.expected, c.err)
+			}
+		})
+	}
+}
+
+func TestRespSerializer_SerializeInteger(t *testing.T) {
+	cases := []struct {
+		data     int64
+		expected []byte
+		err      error
+		message  string
+	}{
+		{
+			data:     127,
+			expected: []byte(":127\r\n"),
+			message:  "Positive integer",
+			err:      nil,
+		},
+	}
+
+	serializer := RespSerializer{}
+	for _, c := range cases {
+		t.Run(c.message, func(t *testing.T) {
+			actual, err := serializer.SerializeInteger(c.data)
+			if !errors.Is(err, c.err) || bytes.Compare(actual, c.expected) != 0 {
+				t.Errorf("Actual data %s, actual error %v, expected data %s, expected error %v", actual, err, c.expected, c.err)
+			}
+		})
+	}
+}
+
+func TestRespSerializer_SerializeError(t *testing.T) {
+	cases := []struct {
+		data     error
+		expected []byte
+		err      error
+		message  string
+	}{
+		{
+			data:     errors.New("unknown error"),
+			expected: []byte("-unknown error\r\n"),
+			message:  "Non empty error",
+			err:      nil,
+		},
+		{
+			data:     nil,
+			expected: nil,
+			message:  "Empty error",
+			err:      EmptyError,
+		},
+	}
+
+	serializer := RespSerializer{}
+	for _, c := range cases {
+		t.Run(c.message, func(t *testing.T) {
+			actual, err := serializer.SerializeError(c.data)
+			if !errors.Is(err, c.err) || bytes.Compare(actual, c.expected) != 0 {
+				t.Errorf("Actual data %s, actual error %v, expected data %s, expected error %v", actual, err, c.expected, c.err)
+			}
+		})
+	}
+}
+
+func TestRespSerializer_SerializeBulkString(t *testing.T) {
+	cases := []struct {
+		data     string
+		expected []byte
+		err      error
+		message  string
+	}{
+		{
+			data:     "normal string",
+			expected: []byte("$13\r\nnormal string\r\n"),
+			message:  "Bulk string with no especial characters",
+			err:      nil,
+		},
+		{
+			data:     "string with\nbreak line",
+			expected: []byte("$22\r\nstring with\nbreak line\r\n"),
+			message:  "Bulk string with break line",
+			err:      nil,
+		},
+	}
+
+	serializer := RespSerializer{}
+	for _, c := range cases {
+		t.Run(c.message, func(t *testing.T) {
+			actual, err := serializer.SerializeBulkString(c.data)
+			if !errors.Is(err, c.err) || bytes.Compare(actual, c.expected) != 0 {
+				t.Errorf("Actual data %s, actual error %v, expected data %s, expected error %v", actual, err, c.expected, c.err)
+			}
+		})
+	}
+}
