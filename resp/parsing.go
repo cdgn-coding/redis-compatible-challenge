1bfbb3967b9c8c94fee8367ef27ede07299b69c9
Carlos David Gonzalez Nexans <cdgn.machines@gmail.com>
2024-09-04 15:30:26 -0300

    implement parser

---
+++ resp/parsing.go
@@ -0,0 +1,98 @@
+package resp
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"strconv"
+)
+
+type RespParser struct{}
+
+var CannotReadDataError = errors.New("cannot read data")
+
+var TypeMismatchError = errors.New("type mismatch")
+
+var UnsupportedType = errors.New("unsupported type")
+
+var NumberOfBytesOff = errors.New("number of bytes off")
+
+func (p RespParser) Parse(data []byte) (interface{}, error) {
+	reader := bytes.NewReader(data)
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanLines)
+	return p.parseWithScanner(scanner)
+}
+
+func (p RespParser) parseWithScanner(scanner *bufio.Scanner) (interface{}, error) {
+	var line []byte
+
+	for scanner.Scan() {
+		line = scanner.Bytes()
+
+		switch line[0] {
+		case '*':
+			restOfLine := string(line[1:])
+			count, err := strconv.Atoi(restOfLine)
+			if err != nil {
+				return nil, errors.Join(err, TypeMismatchError)
+			}
+
+			result := make([]interface{}, count)
+			for i := 0; i < count; i++ {
+				part, err := p.parseWithScanner(scanner)
+
+				if err != nil {
+					return nil, err
+				}
+
+				result[i] = part
+			}
+			return result, nil
+		case ':':
+			restOfLine := string(line[1:])
+			integer, err := strconv.Atoi(restOfLine)
+			if err != nil {
+				return nil, errors.Join(err, TypeMismatchError)
+			}
+
+			return int64(integer), nil
+		case '+':
+			return line[1:], nil
+		case '-':
+			restOfLine := string(line[1:])
+			return errors.New(restOfLine), nil
+		case '$':
+			restOfLine := string(line[1:])
+			totalBytes, err := strconv.Atoi(restOfLine)
+			if err != nil {
+				return nil, errors.Join(err, CannotReadDataError)
+			}
+
+			read := 0
+			result := make([]byte, totalBytes)
+			for read < totalBytes {
+				scanner.Scan()
+				current := scanner.Bytes()
+
+				if current == nil {
+					return nil, errors.Join(CannotReadDataError, NumberOfBytesOff)
+				}
+
+				copy(result[read:read+len(current)], current)
+				read += len(current)
+
+				if read < totalBytes {
+					result[read] = '\n'
+					read += 1
+				}
+			}
+
+			return result, nil
+		default:
+			return nil, UnsupportedType
+		}
+	}
+
+	return nil, CannotReadDataError
+}
---
+++ resp/parsing_test.go
@@ -0,0 +1,105 @@
+package resp
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestRespParser_Parse(t *testing.T) {
+	tests := []struct {
+		name    string
+		data    []byte
+		want    interface{}
+		wantErr bool
+	}{
+		{
+			name:    "Simple integer",
+			data:    []byte(":127\r\n"),
+			want:    int64(127),
+			wantErr: false,
+		},
+		{
+			name:    "Simple string",
+			data:    []byte("+OK\r\n"),
+			want:    []byte("OK"),
+			wantErr: false,
+		},
+		{
+			name:    "Simple error",
+			data:    []byte("-some error"),
+			want:    errors.New("some error"),
+			wantErr: false,
+		},
+		{
+			want:    []byte("normal string"),
+			data:    []byte("$13\r\nnormal string\r\n"),
+			name:    "Bulk string with no especial characters",
+			wantErr: false,
+		},
+		{
+			want:    []byte("string with\nbreak line"),
+			data:    []byte("$22\r\nstring with\nbreak line\r\n"),
+			name:    "Bulk string with break line",
+			wantErr: false,
+		},
+		{
+			want:    nil,
+			data:    []byte("$3000\r\nstring with\nbreak line\r\n"),
+			name:    "Bulk string bad number of bytes",
+			wantErr: true,
+		},
+		{
+			want:    []interface{}{},
+			data:    []byte("*0\r\n"),
+			name:    "Empty array",
+			wantErr: false,
+		},
+		{
+			want: []interface{}{
+				[]byte("hello"),
+				[]byte("world"),
+			},
+			data:    []byte("*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"),
+			name:    "Array two bulk strings",
+			wantErr: false,
+		},
+		{
+			want:    []interface{}{int64(1), int64(2), int64(3)},
+			data:    []byte("*3\r\n:1\r\n:2\r\n:3\r\n"),
+			name:    "Array of three integers",
+			wantErr: false,
+		},
+		{
+			want:    []interface{}{int64(1), int64(2), int64(3), int64(4), []byte("hello")},
+			data:    []byte("*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$5\r\nhello\r\n"),
+			name:    "Array of mixed data types",
+			wantErr: false,
+		},
+		{
+			want:    []interface{}{[]interface{}{int64(1), int64(2), int64(3), int64(4)}, []interface{}{[]byte("hello"), []byte("world")}},
+			data:    []byte("*2\r\n*4\r\n:1\r\n:2\r\n:3\r\n:4\r\n*2\r\n$5\r\nhello\r\n$5\r\nworld\r\n"),
+			name:    "Array of arrays",
+			wantErr: false,
+		},
+		{
+			want:    []interface{}{errors.New("some error")},
+			data:    []byte("*1\r\n-some error\r\n"),
+			name:    "Array with error",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := RespParser{}
+			got, err := p.Parse(tt.data)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Parse() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Parse() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
